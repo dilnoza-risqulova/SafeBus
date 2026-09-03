f030a834e416dd3e5fdbb870020a85d8064b7557
dilnoza-risqulova <dilnoza.08200@gmail.com>
2026-09-03 21:42:02 +0500

    Update Swagger UI endpoint to relative v1/swagger.json

--- EduBus.Api/Program.cs
+++ EduBus.Api/Program.cs
@@ -31,8 +31,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EduBus Safe API v1");
-    c.RoutePrefix = string.Empty;
+    c.SwaggerEndpoint("v1/swagger.json", "EduBus Safe API v1");
+    c.RoutePrefix = "swagger";
 });
 
 app.UseCors("AllowAll");
